bde3d97ee7fe87b8673a1d5c696f4950c3b6fc27
zand <zabahey.sukoi@gmail.com>
2018-02-19 19:08:22 +0700

    create action creator that return an action, an object with type and payload

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -7,7 +7,12 @@ class BookList extends Component {
     renderList() {
         return this.props.books.map(book => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li
+                 key={book.title}
+                 onClick={() => this.props.selectBook(book)} 
+                 className="list-group-item">
+                    {book.title}
+                 </li>
             );
         });
     }
@@ -39,5 +44,5 @@ function mapDispathToProps(dispatch) {
 
 //Promote BookList from a component to a container - it needs to know
 // about this new dispatch method, selectBook.
-// Make it available as aprop.
+// Make it available as a prop.
 export default connect(mapStateToProps, mapDispathToProps)(BookList);
